98ee9be17736e67edcd54c4b0cb2b41e7a006183
Conan Li <conanlics@gmail.com>
2018-07-01 19:40:56 -0400

    no more tap

--- {{cookiecutter.repo_name}}/src/index.tsx
+++ {{cookiecutter.repo_name}}/src/index.tsx
@@ -2,13 +2,9 @@ import * as React from "react";
 import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import createHashHistory from "history/createHashHistory";
-import injectTapEventPlugin from "react-tap-event-plugin";
 
 import { App } from "./containers/App";
 import { configureStore } from "./configureStore";
-
-// WARNING: this line must live in index.js or else will break hot loading
-injectTapEventPlugin();
 
 const history = createHashHistory();
 const store = configureStore(history);
