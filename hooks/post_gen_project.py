19234eb896dac4369630698aabc48233bec0a3ed
Conan Li <conanlics@gmail.com>
2018-07-01 18:51:37 -0400

    add hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+has_frontend = '{{cookiecutter.has_frontend}}' == 'y'
+
+
+if not has_frontend:
+    remove("src")
