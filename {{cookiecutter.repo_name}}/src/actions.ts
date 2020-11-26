794d4939b0c35fefb643fcc92d77f2805ce3b4be
Conan Li <conanlics@gmail.com>
2020-11-25 22:09:30 -0500

    remove frontend

--- {{cookiecutter.repo_name}}/src/actions.ts
+++
@@ -1,5 +0,0 @@
-import { SLOTO_ACTION_CREATORS } from "@src/sloto";
-
-export const ACTIONS = {
-  ...SLOTO_ACTION_CREATORS
-};
--- {{cookiecutter.repo_name}}/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-import * as actionCreators from "@src/actions";
-
-export type ActionType = typeof actionCreators;
-
-export interface State {
-  serverState: {};
-}
